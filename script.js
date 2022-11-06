e7eda596cafa78266fdd67c94f88aee31441127e
Hussein-A4 <smartron4cr7@gmail.com>
2022-11-06 16:48:30 -0500

    edited javascript, added all hard coded id's for each movie, extra css

--- script.js
+++ script.js
@@ -6,26 +6,32 @@ let response = axios.get("https://api.themoviedb.org/3/search/movie", {
   }
 });
 
-response = response.then((moviesData) => {
-  for (let movie of moviesData.data.results) {   
-    axios.get(`https://api.themoviedb.org/3/movie/${movie.id}`, {
+
+var movieId = 0;
+
+function getOption() {
+  selectElement = document.querySelector('#MovieSelector');
+  output = selectElement.value;
+  document.querySelector('.output').textContent = output;
+  console.log(output);
+  movieId = output;
+  axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
       params: {
         api_key: "45f0db8e20f87e3e431aa1750076bb74",
         append_to_response: "videos",
       }
     }).then((movieData) => {
-      const img = document.createElement('img');
-      const p = document.createElement('p');
-      const iframe = document.createElement('iframe');
-
-      const trailers = movieData.data.videos.results.filter((trailer) => trailer.type === "Trailer");
-      iframe.src = `https://www.youtube.com/embed/${trailers.at(0).key}`
-      img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-      p.innerHTML = `${movie.title} -- ${movie.release_date} -- ${movie.popularity}`;
-      
-      document.body.append(p);
-      document.body.append(img);
-      document.body.append(iframe);
+      const img = document.getElementsByClassName('movieImg');
+      const p = document.getElementById('movieTitle');
+      const iframe = document.getElementsByClassName('movieFrame');
+      const movie = movieData.data
+      const trailers = movie.videos.results.filter((trailer) => trailer.type === "Trailer");
+      console.log(p)
+      iframe[0].src = `https://www.youtube.com/embed/${trailers.at(0).key}`
+      img[0].src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+      p.innerHTML = `Movie Title: ${movie.title} -- Release Date:  ${movie.release_date} -- Popularity: ${movie.popularity} -- Average Vote: ${movie.vote_average} -- Vote Count: ${movie.vote_count} -- Original Language: ${movie.original_language} -- Overview: ${movie.overview}`;
     });
-  }
-});
+}
+
+
+
